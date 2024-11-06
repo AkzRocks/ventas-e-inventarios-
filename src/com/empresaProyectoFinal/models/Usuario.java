87319666e92d9ee09faf01e48c3ce705286c6bc6
Wilderakz <navarrowilder88@gmail.com>
2024-11-05 21:35:33 -0500

    rama de diseño establrcidas

--- src/com/empresaProyectoFinal/models/Usuario.java
+++ src/com/empresaProyectoFinal/models/Usuario.java
@@ -4,10 +4,32 @@
  */
 package com.empresaProyectoFinal.models;
 
+import com.empresaProyectoFinal.utils.Permisos;
+
 /**
  *
  * @author will
  */
 public class Usuario {
+    private final String username;
+    private final  Permisos permiso;
+    
+    public Usuario(String username, Permisos permiso){
+        this.username = username;
+        this.permiso = permiso;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Permisos getPermiso() {
+        return permiso;
+    }
+    
+    public boolean obtenerPermiso(String permisoSrt){
+        return this.permiso.toString().equalsIgnoreCase(permisoSrt);
+    }
+    
     
 }
